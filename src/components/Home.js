1ba2c59bec0b276efa5be35625eaa240cbe8fbe2
susanliubc <susanliubc@gmail.com>
2019-01-14 13:21:18 -0800

    fix Slider

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Slider from './Slider'
 import funded from './images/funded.jpg';
 import partner from './images/partner.jpg';
+import './materialize/js/materialize.min';
 
-const Home = (props) => {
-    return (
-        <div className="container">
-            <Slider />
-            <h2 className="title center">Welcome to PCCSH website!</h2>
-            <div>
-                <img className="funded responsive-img center-block" src={funded} alt=""/>
+class Home extends Component {
+    componentDidMount() {
+        $('.slider').slider();
+    };
+    render() {
+        return (
+            <div className="container">
+                <Slider />
+                <h2 className="title center">Welcome to PCCSH website!</h2>
+                <div>
+                    <img className="funded responsive-img center-block" src={funded} alt=""/>
+                </div>
+                <div>
+                    <img className="partner responsive-img center-block" src={partner} alt=""/>
+                </div>
             </div>
-            <div>
-                <img className="partner responsive-img center-block" src={partner} alt=""/>
-            </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Home;
